884b58e2756ed4927758e553850d59180447efdd
Lyjor Barros <lyjoramorim@MacBook-M1-Lyjor.local>
2024-08-13 16:43:13 -0300

    Detalhamento de saida

--- src/main/java/com/exemplo/helloworld/HelloWorldController.java
+++ src/main/java/com/exemplo/helloworld/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World - Curso Docker";
+        return "Hello World - Curso Docker Java/Maven";
     }
 }
